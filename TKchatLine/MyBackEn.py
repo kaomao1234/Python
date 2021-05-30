5fa5efc356e88bac1cdfc77bf6e1bc6364855a0e
kaomao1234 <boripats63@nu.ac.th>
2021-05-30 23:59:11 +0700

    Create MyBackEn.py

---
+++ TKchatLine/MyBackEn.py
@@ -0,0 +1,31 @@
+import json
+import os
+import pprint
+import time
+import multiprocessing as mp
+from flask import Flask, request, make_response
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def MainProcess():
+    get_intexd_dialgflow = request.get_json(silent=True, force=True)
+    # pprint.pprint(get_intexd_dialgflow)
+    ans_from_user = get_intexd_dialgflow['originalDetectIntentRequest']['payload']['data']['message']['text']
+    return send_user(ans_from_user)
+
+
+def require_ans(text):
+    return json.dumps({"fulfillmentText": 'Nothing'}, indent=4)
+
+
+def send_user(ans_from_user):
+    res = make_response(require_ans(ans_from_user))
+    res.headers['Content-Type'] = 'application/json'
+    return res
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print(f'Starting app on port {port}')
+    app.run(debug=False, port=port, host='0.0.0.0', threaded=True)
