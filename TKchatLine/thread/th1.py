2032d44435f5c85da508850f20560d92f5cf561d
kaomao1234 <boripats63@nu.ac.th>
2021-06-02 23:02:02 +0700

    add all

---
+++ TKchatLine/thread/th1.py
@@ -0,0 +1,13 @@
+import threading as th
+import  time
+def func_work(work:int,delay:float):
+    for i in range(10):
+        print(f"work{work} : {i}")
+        time.sleep(delay)
+w1 = th.Thread(target= func_work ,args=(1,1.0))
+w2 = th.Thread(target= func_work ,args=(2,0.5))
+w1.start()
+w2.start()
+w1.join()
+w2.join()
+print('----------finish-------'.rjust(20))
