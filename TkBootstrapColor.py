79f3880f51d89c3b18da2b904f2e6750ecbae103
kaomao1234 <boripats63@nu.ac.th>
2021-08-02 00:34:58 +0700

    กุว่าง

---
+++ TkBootstrapColor.py
@@ -0,0 +1,44 @@
+from tkinter import *
+from ast import *
+import re
+
+
+class ColorSample(Tk):
+    def __init__(self):
+        super().__init__()
+        self.color = {'blue':       '#467FD0',
+                      'indigo':     '#6610f2',
+                      'purple':     '#7c69ef',
+                      'pink':       '#e83e8c',
+                      'red':        '#df4759',
+                      'orange':     '#fd9644',
+                      'yellow':     '#ffc107',
+                      'green':      '#42ba96',
+                      'teal':       '#20c997',
+                      'cyan':       '#17a2b8',
+                      'light-blue': '#69d2f1',
+                      'white': '#FFFFFF',
+                      'gray-base': '#181b1e',
+                      'gray-100': '#F9FBFD',
+                      'gray-200': '#F1F4F8',
+                      'gray-300': '#D9E2EF',
+                      'gray-400': '#C6D3E6',
+                      'gray-500': '#ABBCD5',
+                      'gray-600': '#869AB8',
+                      'gray-700': '#506690',
+                      'gray-800': '#384C74',
+                      'gray-900': '#1B2A4E',
+                      'black': '#161C2D'}
+        for i in self.color:
+            Label(self, text=i, background=self.color[i], font='consolas 15').grid(
+                sticky='ew')
+
+    def regColor(self, m):
+        m = m.split(';')
+        m = [i.replace(' !default', '').replace(
+            '$', '').replace('\n', '') for i in m]
+        m = {i.split(': ')[0]: i.split(' : ')[1] for i in m}
+
+
+if __name__ == '__main__':
+    ColorSample().mainloop()
