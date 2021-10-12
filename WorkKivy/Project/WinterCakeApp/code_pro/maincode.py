0f5f3caee9f34f67e970e0874b9a090cf124b91d
Beginner-Project <boripats63@nu.ac.th>
2021-10-13 02:12:23 +0700

    add all

--- WorkKivy/Project/WinterCakeApp/code_pro/maincode.py
+++ WorkKivy/Project/WinterCakeApp/code_pro/maincode.py
@@ -16,9 +16,10 @@ from kivymd.uix.screen import MDScreen
 from kivymd.app import MDApp
 from functools import partial
 from pprint import pprint
+from kivy.uix.button import Button
 from kivy.properties import ColorProperty
-
-lst_file = ["onboarding.kv", 'sign_in.kv', 'sign_up.kv']
+from kivy.core.window import Window
+lst_file = ["onboarding.kv", 'sign_in.kv', 'sign_up.kv', 'shopping.kv']
 for i in lst_file:
     Builder.load_file("{}".format(i))
 
@@ -37,10 +38,13 @@ class MainScreen(ScreenManager):
     def add_layout_with_id(self):
         sign_in = Sign_in
         sign_up = Sign_up
+        shopping = ShopScreen
         self.ids[sign_in.id] = sign_in(self)
         self.ids[sign_up.id] = sign_up(self)
+        self.ids[shopping.id] = shopping(self)
         self.ids.s_drop_manager.add_widget(self.ids[sign_in.id])
         self.ids.s_drop_manager.add_widget(self.ids[sign_up.id])
+        self.add_widget(self.ids[shopping.id])
 
     def start(self, *e):
         p = Animation(y=0, duration=0.2)
@@ -51,6 +55,7 @@ class MainScreen(ScreenManager):
 
 class Sign_in(MDScreen):
     id = 'sign_in'
+
     def __init__(self, root, **kwargs):
         super(Sign_in, self).__init__(**kwargs)
         self.ids.eye_pass.bind(state=partial(
@@ -60,9 +65,11 @@ class Sign_in(MDScreen):
             icon='arrow-expand-left',
             size_hint=(.1, .1),
             pos_hint={'x': 0, 'y': .9},
+            text='back',
             theme_text_color="Custom",
             line_color=self.copper_rust,
-            icon_color=self.white
+            icon_color=self.white,
+            text_color=self.white,
         )
 
     def switch_event(self, text_event, *e):
@@ -75,15 +82,20 @@ class Sign_in(MDScreen):
             instance.icon = "eye-off"
             text_event.password = True
 
-    def next_screen(self):
+    def next_sign_up(self):
         self.manager.transition.direction = 'left'
         self.manager.transition.duration = 0.5
         self.manager.current = 'sign_up'
         anim = Animation(size_hint_y=0.7, duration=0.2)
         anim.start(self.root.ids.backdrop_widget)
-        self.back_arrow.on_press = self.root.ids.sign_up.back_screen
+        self.back_arrow.on_press = self.root.ids.sign_up.back_to_sign_in
         self.root.ids.on_board.add_widget(self.back_arrow)
         self.root.ids.sign_up.back_arrow = self.back_arrow
+
+    def next_shop_page(self):
+        self.root.transition.direction = 'left'
+        self.root.transition.duration = 0.2
+        self.root.current = 'shop_screen'
 
 
 class Sign_up(MDScreen):
@@ -98,7 +110,7 @@ class Sign_up(MDScreen):
         self.ids.eye_confirm_pass.bind(state=partial(
             self.switch_event, self.ids.confirm_password_field))
 
-    def back_screen(self, *e):
+    def back_to_sign_in(self, *e):
         self.manager.transition.direction = 'right'
         self.manager.transition.duration = 0.5
         self.manager.current = 'sign_in'
@@ -115,6 +127,28 @@ class Sign_up(MDScreen):
         else:
             instance.icon = "eye-off"
             text_event.password = True
+
+
+class ShopScreen(MDScreen):
+    id = 'shopping'
+    
+    def __init__(self, root, **kwargs):
+        super(ShopScreen, self).__init__(**kwargs)
+        self.root = root
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+        for i in range(1,21):
+            self.ids.shop_view.add_widget(Button(text=f"Product {i}"))
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'c':
+            self.root.transition.direction = 'right'
+            self.root.transition.duration = 0.2
+            self.root.current = 'on_board_screen'
 
 
 class WinterCake(MDApp):
--- WorkKivy/Project/WinterCakeApp/widget_demo/Text.py
+++ WorkKivy/Project/WinterCakeApp/widget_demo/Text.py
@@ -12,38 +12,85 @@ Builder.load_string("""
     spacing: '10dp'
     AnchorLayout:
         anchor_y: 'center'
-        GridLayout:
-            size_hint: None,None 
-            cols:2
-            size:300,10
-            MDTextField:
+        MDBoxLayout:
+            orientation:'vertical'
+            size_hint_y: None
+            AnchorLayout:
+                anchor_x:'center'
                 size_hint_y: None
-                id:t_widget
-                text:"Hello world"
-                password:True
-            MDIconButton:
+                height:23
+                MDIconButton:
+                    size_hint_y: None
+                    icon:'store-plus'
+                    user_font_size:"20dp"
+                    theme_text_color: "Custom"
+                # text_color:root.copper_rust
+            Label:
                 size_hint_y: None
-                id:hide
-                icon:"eye"
-                ripple_scale:0
+                height:15
+                text:'halo'
+                # font_name:root.sukhumvit_font
+                markup:True
+                font_size:14
+                # halign:'center'
+                color:0,0,0,1
+                # color:root.copper_rust
+                # text_size:self.size
+        # MDBoxLayout:
+        #     id:parent
+        #     size_hint: None,None 
+        #     # size_hint_y: None
+        #     cols:2
+        #     size:300,40
+        #     padding:[18,0]
+        #     radius:[18,18]
+        #     orientation:'horizontal'
+        #     line_color:get_color_from_hex("#9b4f4f")
+        #     MDTextField:
+        #         size_hint_y:None
+        #         -height:50
+        #         # line_anim:False
+        #         id:t_widget
+        #         text:"hello world"
+        #         # password:True
+        #         text_color:0,0,0,1
+        #         line_color_focus: 0,0,0,1
+        #     MDIconButton:
+        #         size_hint_y: None
+        #         size_hint_x:None
+        #         id:hide
+        #         icon:"magnify"
+        #         user_font_size:"18dp"
+        #         theme_text_color: "Custom"
+        #         # text_color:get_color_from_hex("#9b4f4f")
+        #         ripple_scale:0
+        #         ripple_alpha:0
+        #         ripple_rad_default:0
 """)
 class M(BoxLayout):
     def __init__(self, **kwargs):
         super(M, self).__init__(**kwargs)
-        pprint(dir(self.ids.hide))
+        # pprint(dir(self.ids.hide))
         # print(self.ids.Field_btn.focus)
-        self.ids.hide.bind(state=self.on_press)
+        # self.ids.hide.bind(state=self.on_press)
+        # pprint(dir(self.ids.t_widget))
     
     
-    def on_press(self, *e):
-        focus = e[1]
-        instance = e[0]
-        if focus == "normal":
-            instance.icon = "eye"
-            self.ids.t_widget.password = True
-        else:
-            instance.icon = "eye-off"
-            self.ids.t_widget.password = False
+    # def on_press(self, *e):
+    #     focus = e[1]
+    #     instance = e[0]
+    #     print(instance.ripple_duration_in_fast)
+    #     print(instance.ripple_duration_in_slow)
+    #     print(instance.ripple_duration_out)
+    #     print(instance.ripple_func_in)
+    #     print(instance.ripple_func_out)
+    #     print(instance.ripple_rad_default)
+    #     if focus == "normal":
+    #         instance.icon = "eye"
+    #         self.ids.t_widget.password = True
+    #     else:
+    #         instance.icon = "eye-off"
+    #         self.ids.t_widget.password = False
 
 class Sene(Screen):
     def __init__(self, **kw):
