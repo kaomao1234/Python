2dfec146c3257e86a4330e646dc86100c51c291d
Beginner-Project <boripats63@nu.ac.th>
2021-11-14 00:42:50 +0700

    add all

--- Kivy-learning/workshop/Tech_marketplace/main.py
+++ Kivy-learning/workshop/Tech_marketplace/main.py
@@ -5,11 +5,19 @@ from kivy.lang import Builder
 from lib.Root.root import Root
 from lib.Onboard.onboard import OnboardScreen
 from lib.component.Tab.tab import Tab
+from lib.Onboard.lib.Connexion.connexion import ConnexionScreen
+from lib.Onboard.lib.Connexion.lib.ConnectScreen.connectscreen import ConnectScreen
+from lib.Onboard.lib.Connexion.lib.RegisterScreen.registerscreen import RegisterScreen
+from lib.Onboard.lib.Connexion.lib.LoginScreen.loginscreen import LoginScreen
 Window.size = (360, 640)
 path = {
         os.path.join(os.getcwd(),'lib','Root','root.kv'),
         os.path.join(os.getcwd(),'lib','Onboard','onboard.kv'),
-}
+        os.path.join(os.getcwd(),'lib','Onboard','lib','Connexion','connexion.kv'),
+        os.path.join(os.getcwd(),'lib','Onboard','lib','Connexion','lib','ConnectScreen','connectscreen.kv'),
+        os.path.join(os.getcwd(),'lib','Onboard','lib','Connexion','lib','RegisterScreen','registerscreen.kv'),
+        os.path.join(os.getcwd(),'lib','Onboard','lib','Connexion','lib','LoginScreen','loginscreen.kv')
+    }
 for i in path:
     Builder.load_file(i)
 class MyApp(MDApp):
