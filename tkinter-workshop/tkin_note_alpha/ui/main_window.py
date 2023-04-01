f9a66968c2cde42ada0eae9754d041994bbcedc4
Beginner-Project <boripats63@nu.ac.th>
2023-04-01 09:40:19 +0700

    add all

--- tkinter-workshop/tkin_note_alpha/ui/main_window.py
+++ tkinter-workshop/tkin_note_alpha/ui/main_window.py
@@ -5,6 +5,7 @@ from ui.toolbar import ToolBar
 
 class MainWindow(ttk.Frame):
     def __init__(self, master) -> None:
-        super().__init__(master,style=DANGER)
-        ToolBar(self,style=LIGHT).grid(row=0,column=0,columnspan=2,sticky="NSEW")
-        self.columnconfigure(0,weight=1)
+        super().__init__(master, style=DANGER)
+        self.toolbar = ToolBar(self, style=LIGHT)
+        self.toolbar.grid(row=0, column=0, columnspan=2, sticky="NSEW")
+        self.columnconfigure(0, weight=1)
--- tkinter-workshop/tkin_note_alpha/ui/toolbar.py
+++ tkinter-workshop/tkin_note_alpha/ui/toolbar.py
@@ -1,18 +1,9 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+from threading import Thread
 
 
 class ToolBar(ttk.Frame):
     def __init__(self, master, style) -> None:
         super().__init__(master, height=22, style=style)
 
-        self.bind("<Configure>", self.on_size)
-
-    def on_size(self, *args):
-        self.master.master.update()
-        print(self.winfo_width(), self.winfo_height())
-        if (self.winfo_width() <= 120):
-            self.configure(height=66)
-        else:
-            self.configure(height=22)
-        
