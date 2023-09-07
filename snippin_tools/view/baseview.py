1b64b1e0768b2b61248a42af343b7732c5d21aac
Beginner-Project <boripats63@nu.ac.th>
2023-09-07 13:50:37 +0700

    update

--- snippin_tools/view/baseview.py
+++ snippin_tools/view/baseview.py
@@ -12,10 +12,18 @@ class Sized:
 
 
 class BaseView:
+    """A base class for managing views in a GUI application."""
     pages: dict = {}
     sized: Sized = None
 
     def __init__(self, root: ft.Page, route):
+        """
+        Initialize a BaseView instance.
+
+        Args:
+            root (ft.Page): The root page of the GUI.
+            route: The initial route for this view.
+        """
         self.root = root
         self.route = route
         self.root.on_route_change = self.route_change
@@ -27,6 +35,12 @@ class BaseView:
             BaseView.pages[route] = self.get_view()
 
     def route_change(self, route_event: ft.RouteChangeEvent):
+        """
+        Handle a route change event.
+
+        Args:
+            route_event (ft.RouteChangeEvent): The route change event.
+        """
         self.root.views.clear()
         if route_event.route in self.pages.keys():
             self.root.views.append(self.pages[route_event.route])
@@ -35,7 +49,13 @@ class BaseView:
     def get_view(self) -> View:
         return ft.View()
 
-    def setState(self,event=None):
+    def update_view_state(self,event=None):
+        """
+        Set the state of the view.
+
+        Args:
+            event: An optional event to trigger the state change.
+        """
         event() if event != None else event
         self.root.views.remove(self.pages[self.route])
         self.pages[self.route] = self.get_view()
@@ -49,7 +69,7 @@ class BaseView:
 
     def on_resize(self, e: ft.ControlEvent):
         self.sized = Sized(self.root.width, self.root.height)
-        self.setState()
+        self.update_view_state()
 
     def on_window_event(self, e:ft.ControlEvent):
         pass
--- snippin_tools/view/main_view.py
+++ snippin_tools/view/main_view.py
@@ -8,7 +8,6 @@ class MainView(BaseView):
         self.option_selected = "Mode"
         self.val = 0
         super().__init__(root, route)
-       
 
     def on_dropdown_change(self, e: ft.ControlEvent):
         self.option_selected = e.data
@@ -16,16 +15,34 @@ class MainView(BaseView):
 
     def get_view(self):
         print("setState")
-        return ft.View(route=self.route, controls=[
-            ft.Row(height=56, controls=[
-                ft.TextButton("New", on_click=lambda *e: self.setState(lambda: setattr(self, 'val', self.val+1)), height=self.root.height, width=150,
-                              style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=ft.BorderRadius(8, 8, 8, 8)))),
-                ft.Dropdown(hint_text="Mode", on_change=self.on_dropdown_change, width=200, options=[
-                    ft.dropdown.Option("Free-from Snip"),
-                    ft.dropdown.Option("Rectangular Snip"),
-                    ft.dropdown.Option("Window Snip"),
-                    ft.dropdown.Option("Full-screen Snip")
-                ]),
-                ft.Text(str(self.val))
-            ])
-        ])
+        return ft.View(
+            route=self.route,
+            controls=[
+                ft.Row(
+                    height=56,
+                    controls=[
+                        ft.TextButton(
+                            "New",
+                            on_click=lambda *e: self.update_view_state(
+                                lambda: setattr(self, 'val', self.val + 1)),
+                            height=self.root.height*0.5,
+                            width=150,
+                            style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(
+                                radius=ft.BorderRadius(8, 8, 8, 8)))
+                        ),
+                        ft.Dropdown(
+                            hint_text="Mode",
+                            on_change=self.on_dropdown_change,
+                            width=200,
+                            options=[
+                                ft.dropdown.Option("Free-form Snip"),
+                                ft.dropdown.Option("Rectangular Snip"),
+                                ft.dropdown.Option("Window Snip"),
+                                ft.dropdown.Option("Full-screen Snip")
+                            ]
+                        ),
+                        ft.Text(str(self.val))
+                    ]
+                )
+            ]
+        )
